7cd65891deedd4fcf37cead4435e18d25217b16a
Kayxue <s0990006@gmail.com>
2020-06-20 22:31:03 +0800

    2020-06-20

---
+++ 資訊專題/pauseform.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace 資訊專題
+{
+    partial class pauseform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.backtomenu = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.continuegame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(60, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "遊戲暫停";
+            // 
+            // backtomenu
+            // 
+            this.backtomenu.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.backtomenu.Location = new System.Drawing.Point(47, 172);
+            this.backtomenu.Name = "backtomenu";
+            this.backtomenu.Size = new System.Drawing.Size(132, 37);
+            this.backtomenu.TabIndex = 2;
+            this.backtomenu.Text = "回主畫面";
+            this.backtomenu.UseVisualStyleBackColor = true;
+            this.backtomenu.Click += new System.EventHandler(this.backtomenu_Click);
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.restart.Location = new System.Drawing.Point(47, 129);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(132, 37);
+            this.restart.TabIndex = 1;
+            this.restart.Text = "重新開始";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // continuegame
+            // 
+            this.continuegame.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.continuegame.Location = new System.Drawing.Point(47, 86);
+            this.continuegame.Name = "continuegame";
+            this.continuegame.Size = new System.Drawing.Size(132, 37);
+            this.continuegame.TabIndex = 0;
+            this.continuegame.Text = "繼續遊戲";
+            this.continuegame.UseVisualStyleBackColor = true;
+            this.continuegame.Click += new System.EventHandler(this.continuegame_Click);
+            // 
+            // pauseform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 221);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backtomenu);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.continuegame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "pauseform";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "遊戲暫停中......";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.pauseform_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button backtomenu;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button continuegame;
+    }
+}
---
+++ 資訊專題/pauseform.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 資訊專題
+{
+    public partial class pauseform : Form
+    {
+        gameform gameform = new gameform();
+        PrivateFontCollection fontcollection = new PrivateFontCollection();
+        bool buttonclicked = false;
+        public pauseform(gameform game)
+        {
+            InitializeComponent();
+            int distance;
+            gameform = game;
+            fontcollection.AddFontFile(Application.StartupPath + "\\新特明體.TTC");
+            Font font = new Font(fontcollection.Families[0], 18);
+            label1.Font = font;
+            continuegame.Font = font;
+            restart.Font = font;
+            backtomenu.Font = font;
+            label1.Left = ((this.Width - 20) / 2) - (label1.Width / 2);
+            continuegame.Left = ((this.Width - 20) / 2) - (continuegame.Width / 2);
+            restart.Left = ((this.Width - 20) / 2) - (restart.Width / 2);
+            backtomenu.Left = ((this.Width - 20) / 2) - (backtomenu.Width / 2);
+            distance = (this.Height - 40 - label1.Height - continuegame.Height - restart.Height - backtomenu.Height) / 5;
+            label1.Top = distance;
+            continuegame.Top = label1.Top + label1.Height + distance;
+            restart.Top = continuegame.Top + continuegame.Height + distance;
+            backtomenu.Top = restart.Top + restart.Height + distance;
+
+        }
+
+        private void continuegame_Click(object sender, EventArgs e)
+        {
+            buttonclicked = true;
+            this.Close();
+            gameform.getresult = "startgame";
+            this.Dispose();
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            buttonclicked = true;
+            this.Close();
+            gameform.getresult = "again";
+            this.Dispose();
+        }
+
+        private void backtomenu_Click(object sender, EventArgs e)
+        {
+            buttonclicked = true;
+            this.Close();
+            gameform.getresult = "backtomenu";
+            this.Dispose();
+        }
+
+        private void pauseform_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (buttonclicked == false)
+            {
+                gameform.getresult = "startgame";
+                this.Dispose();
+            }
+        }
+    }
+}
