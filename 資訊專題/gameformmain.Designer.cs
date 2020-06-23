5d20dc06a527b2a3daa02d13715171d148bd9cc8
Kayxue <s0990006@gmail.com>
2020-06-23 20:19:39 +0800

    2020-06-23

--- 資訊專題/gameformmain.Designer.cs
+++ 資訊專題/gameformmain.Designer.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:資訊專題/gameformmain.Designer.cs
 ﻿namespace 資訊專題
 {
     partial class gameformmain
@@ -186,4 +187,199 @@
         private System.Windows.Forms.Timer timer4;
         private System.Windows.Forms.Timer timer3;
     }
+=======
+﻿namespace 資訊專題
+{
+    partial class gameform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.debugtext = new System.Windows.Forms.Label();
+            this.myheart = new System.Windows.Forms.ProgressBar();
+            this.bossheart = new System.Windows.Forms.ProgressBar();
+            this.bosshearttext = new System.Windows.Forms.Label();
+            this.myhearttext = new System.Windows.Forms.Label();
+            this.me = new System.Windows.Forms.PictureBox();
+            this.boss = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bossmove = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.me)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boss)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // debugtext
+            // 
+            this.debugtext.AutoSize = true;
+            this.debugtext.Location = new System.Drawing.Point(61, 44);
+            this.debugtext.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.debugtext.Name = "debugtext";
+            this.debugtext.Size = new System.Drawing.Size(80, 16);
+            this.debugtext.TabIndex = 2;
+            this.debugtext.Text = "debugtext";
+            this.debugtext.Visible = false;
+            this.debugtext.TextChanged += new System.EventHandler(this.debugtext_TextChanged);
+            // 
+            // myheart
+            // 
+            this.myheart.Location = new System.Drawing.Point(0, 570);
+            this.myheart.Margin = new System.Windows.Forms.Padding(4);
+            this.myheart.Name = "myheart";
+            this.myheart.Size = new System.Drawing.Size(933, 31);
+            this.myheart.TabIndex = 3;
+            this.myheart.Value = 100;
+            // 
+            // bossheart
+            // 
+            this.bossheart.Location = new System.Drawing.Point(0, -1);
+            this.bossheart.Margin = new System.Windows.Forms.Padding(4);
+            this.bossheart.Name = "bossheart";
+            this.bossheart.Size = new System.Drawing.Size(933, 31);
+            this.bossheart.TabIndex = 4;
+            this.bossheart.Value = 100;
+            // 
+            // bosshearttext
+            // 
+            this.bosshearttext.AutoSize = true;
+            this.bosshearttext.BackColor = System.Drawing.Color.Transparent;
+            this.bosshearttext.Location = new System.Drawing.Point(1006, 44);
+            this.bosshearttext.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bosshearttext.Name = "bosshearttext";
+            this.bosshearttext.Size = new System.Drawing.Size(56, 16);
+            this.bosshearttext.TabIndex = 5;
+            this.bosshearttext.Text = "label1";
+            // 
+            // myhearttext
+            // 
+            this.myhearttext.AutoSize = true;
+            this.myhearttext.BackColor = System.Drawing.Color.Transparent;
+            this.myhearttext.Location = new System.Drawing.Point(1006, 539);
+            this.myhearttext.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.myhearttext.Name = "myhearttext";
+            this.myhearttext.Size = new System.Drawing.Size(56, 16);
+            this.myhearttext.TabIndex = 6;
+            this.myhearttext.Text = "label2";
+            // 
+            // me
+            // 
+            this.me.BackColor = System.Drawing.Color.Transparent;
+            this.me.Location = new System.Drawing.Point(64, 266);
+            this.me.Name = "me";
+            this.me.Size = new System.Drawing.Size(119, 211);
+            this.me.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.me.TabIndex = 9;
+            this.me.TabStop = false;
+            // 
+            // boss
+            // 
+            this.boss.BackColor = System.Drawing.Color.Transparent;
+            this.boss.Location = new System.Drawing.Point(753, 178);
+            this.boss.Name = "boss";
+            this.boss.Size = new System.Drawing.Size(292, 299);
+            this.boss.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boss.TabIndex = 8;
+            this.boss.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bossmove
+            // 
+            this.bossmove.AutoSize = true;
+            this.bossmove.BackColor = System.Drawing.Color.Transparent;
+            this.bossmove.Location = new System.Drawing.Point(61, 86);
+            this.bossmove.Name = "bossmove";
+            this.bossmove.Size = new System.Drawing.Size(72, 16);
+            this.bossmove.TabIndex = 10;
+            this.bossmove.Text = "bossmove";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // gameform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1067, 600);
+            this.Controls.Add(this.bossmove);
+            this.Controls.Add(this.me);
+            this.Controls.Add(this.boss);
+            this.Controls.Add(this.myhearttext);
+            this.Controls.Add(this.bosshearttext);
+            this.Controls.Add(this.bossheart);
+            this.Controls.Add(this.myheart);
+            this.Controls.Add(this.debugtext);
+            this.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "gameform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "勇者鬥惡龍";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.gameform_FormClosed);
+            this.Load += new System.EventHandler(this.gameform_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameform_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameform_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.me)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boss)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label debugtext;
+        private System.Windows.Forms.ProgressBar myheart;
+        private System.Windows.Forms.ProgressBar bossheart;
+        private System.Windows.Forms.Label bosshearttext;
+        private System.Windows.Forms.Label myhearttext;
+        private System.Windows.Forms.PictureBox boss;
+        private System.Windows.Forms.PictureBox me;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label bossmove;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer2;
+    }
+>>>>>>> parent of c299013... 2020-06-23:資訊專題/gameform.Designer.cs
 }
--- 資訊專題/gameformmain.cs
+++ 資訊專題/gameformmain.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:資訊專題/gameformmain.cs
 ﻿using System;
 using System.Media;
 using System.Collections.Generic;
@@ -352,3 +353,362 @@ namespace 資訊專題
         }
     }
 }
+=======
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace 資訊專題
+{
+    public partial class gameform : Form
+    {
+        /*宣告變數*/
+        int memove, bossmoveint, nowmeimage, meimagecount = 3;
+        bool needagain, block, attack, resume;
+        Form1 form1 = new Form1();
+        gameformbackground gameformbackground = new gameformbackground();
+        Point x;
+        Image[,] meimages;
+        Image[,] bossimages;
+        WindowsMediaPlayer wmplayer = new WindowsMediaPlayer();
+        SoundPlayer meattacksound = new SoundPlayer();
+        PrivateFontCollection fontcollection = new PrivateFontCollection();
+        Random random = new Random();
+
+        /*初始化與視窗關閉*/
+        public gameform()
+        {
+            InitializeComponent();
+            meattacksound.SoundLocation = Application.StartupPath + "\\";
+            this.KeyPreview = true;
+            myhearttext.Text = myheart.Maximum.ToString();
+            bosshearttext.Text = bossheart.Maximum.ToString();
+            fontcollection.AddFontFile(Application.StartupPath + "\\新特明體.TTC");
+            Font font = new Font(fontcollection.Families[0], 20);
+            this.Font = font;
+            block = false;
+            bossmove.Text = "left";
+            meimages = new Image[,] {
+                { Image.FromFile("主角 站立.png"), Image.FromFile("主角 格擋1.png"), Image.FromFile("主角 格擋2.png") },
+                { Image.FromFile("主角 站立1.png"), Image.FromFile("主角 格擋1(1).png"), Image.FromFile("主角 格擋2(1).png") } 
+            };
+            bossimages = new Image[,] { 
+                { Image.FromFile("魔王 初始.png"), Image.FromFile("魔王 攻擊1.png"), Image.FromFile("魔王 攻擊2.png") }, 
+                { Image.FromFile("魔王 初始(1).png"), Image.FromFile("魔王 攻擊1(1).png"), Image.FromFile("魔王 攻擊2(1).png") } 
+            };
+            nowmeimage = 0;
+            resume = false;
+        }
+
+        private void gameform_Load(object sender, EventArgs e)
+        {
+            this.Location = x;
+            this.BackColor = Color.White;
+            this.TransparencyKey = this.BackColor;
+            me.Image = meimages[0,0];
+            boss.Image = bossimages[0, 0];
+            myheart.Size = new Size(this.Width, myheart.Height);
+            bossheart.Size = new Size(this.Width, myheart.Height);
+            myheart.Top = this.Height - myheart.Height;
+            myhearttext.Left = this.Width - myhearttext.Width - 20;
+            myhearttext.Top = this.Height - 110 - myhearttext.Height;
+            bosshearttext.Left = this.Width - bosshearttext.Width - 20;
+            me.Top = this.Height - me.Height - 150;
+            boss.Top = this.Height - boss.Height - 100;
+            debugtext.Text = "startgame";
+            bossmoveint = 0;
+        }
+
+
+        private void gameform_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (needagain == false)
+            {
+                form1.Visible = true;
+                gameformbackground.Close();
+                wmplayer.close();
+            }
+        }
+
+        /*遊戲監聽器*/
+        private void debugtext_TextChanged(object sender, EventArgs e)
+        {
+            if (debugtext.Text == "startgame")
+            {
+                if(resume == false)
+                {
+                    wmplayer.URL = Application.StartupPath + "\\遊戲中背景音樂.wav";
+                    wmplayer.settings.setMode("loop", true);
+                }
+                else
+                {
+                    wmplayer.controls.play();
+                }
+                this.TopMost = true;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                timer3.Enabled = true;
+            }
+            else if (debugtext.Text == "gamepause")
+            {
+                pauseform pauseform = new pauseform(this);
+
+                wmplayer.controls.pause();
+                pauseform.ShowDialog();
+            }
+            else if(debugtext.Text == "again")
+            {
+                form1.getinfofromgame = "again";
+                debugtext.Text = "again";
+                needagain = true;
+                wmplayer.close();
+                gameformbackground.Close();
+                this.Close();
+                this.Dispose();
+            }
+            else if(debugtext.Text == "backtomenu")
+            {
+                debugtext.Text = "backtomenu";
+                gameformbackground.Close();
+                this.Close();
+                needagain = false;
+                form1.Visible = true;
+                wmplayer.close();
+                this.Dispose();
+            }
+            else if(debugtext.Text == "endgame")
+            {
+
+                wmplayer.close();
+                debugtext.Text="endgame";
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                endform endform = new endform(form1, this);
+                endform.getfromlocation = x;
+                endform.ShowDialog();
+            }
+        }
+
+        /*鍵盤控制*/
+        private void gameform_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(debugtext.Text == "startgame")
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    timer4.Enabled = true;
+                    timer5.Enabled = false;
+                    block = true;
+                }
+                if(block == false)
+                {
+                    if (e.KeyCode == Keys.Left)
+                    {
+                        if (me.Left - 10 >= 0)
+                        {
+                            me.Left -= 10;
+                            memove = 1;
+                            me.Image = meimages[memove, 0];
+                            block = false;
+                        }
+                    }
+                    if (e.KeyCode == Keys.Right)
+                    {
+                        if (me.Left + 10 <= this.Width - 20 - me.Width)
+                        {
+                            me.Left += 10;
+                            memove = 0;
+                            me.Image = meimages[memove, 0];
+                            block = false;
+                        }
+                    }
+                    if (e.KeyCode == Keys.Z)
+                    {
+                        if (me.Left + me.Width > boss.Left && me.Left < boss.Left + boss.Width && attack == false)
+                        {
+                            block = false;
+                            attack = true;
+
+                            if (bossheart.Value - 10 > 0)
+                            {
+                                bossheart.Value -= 10;
+                                bosshearttext.Text = bossheart.Value.ToString();
+                            }
+                            else
+                            {
+                                bossheart.Maximum += 100;
+                                bossheart.Value = bossheart.Maximum;
+                                bosshearttext.Text = bossheart.Value.ToString();
+                                boss.Left = random.Next(5, this.Width - boss.Width - 10);
+                            }
+                            meattacksound.Play();
+                        }
+                    }
+                    if (e.KeyCode == Keys.P)
+                    {
+                        debugtext.Text = "gamepause";
+                    }
+                }
+            }
+        }
+
+        /*結束格檔與攻擊*/
+        private void gameform_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (debugtext.Text == "startgame")
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    timer4.Enabled = false;
+                    timer5.Enabled = true;
+                }
+                else if (e.KeyCode == Keys.Z)
+                {
+                    attack = false;
+                }
+            }
+        }
+
+        /*計時器*/
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(debugtext.Text == "startgame")
+            {
+                boss.Image = bossimages[bossmoveint, 0];
+                if(bossmove.Text == "left")
+                {
+                
+                    if (boss.Left - 10 >= 0)
+                    {
+                        bossmoveint = 0;
+                        boss.Left -= 10;
+                        if(boss.Left < me.Left + me.Width)
+                        {
+                        
+                        }
+                    }
+                }
+                else if(bossmove.Text == "right")
+                {
+                
+                    if (boss.Left + 10 <= this.Width - 20 - boss.Width)
+                    {
+                        bossmoveint = 1;
+                        boss.Left += 10;
+                        if (boss.Left + boss.Width > me.Left)
+                        {
+                        
+                        }
+                    }
+                }
+            }
+                
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (me.Left + me.Width < boss.Left)
+            {
+                bossmove.Text = "left";
+            }
+            else if (me.Left > boss.Left + boss.Width)
+            {
+                bossmove.Text = "right";
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            //nowmeimage現在圖示編號
+            //block是否處於格擋
+            //meimage[方向編號,造型編號]
+            if(nowmeimage == -1)
+            {
+                nowmeimage = 0;
+            }
+            if(block)
+            {
+                if (nowmeimage < meimagecount)
+                {
+                    me.Image = meimages[memove, nowmeimage];
+                    nowmeimage += 1;
+                }
+                else
+                {
+                    timer4.Enabled = false;
+                }
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (block)
+            {
+                nowmeimage -= 1;
+                if (nowmeimage > 0)
+                {
+                    me.Image = meimages[memove, nowmeimage];
+                    nowmeimage -= 1;
+                }
+                else
+                {
+                    timer5.Enabled = false;
+                    block = false;
+
+                }
+            } 
+        }
+
+        /*視窗間傳值*/
+        public Form1 GetForm1
+        {
+            set
+            {
+                form1 = value;
+            }
+        }
+
+        public gameformbackground GetGameformbackground
+        {
+            set
+            {
+                gameformbackground = value;
+            }
+        }
+
+        public string getresult
+        {
+            set
+            {
+                debugtext.Text = value;
+            }
+        }
+
+        public Size returnthisformsize
+        {
+            get
+            {
+                return this.Size;
+            }
+        }
+
+        public Point getfromlocation
+        {
+            set
+            {
+                x = value;
+            }
+        }
+    }
+}
+>>>>>>> parent of c299013... 2020-06-23:資訊專題/gameform.cs
