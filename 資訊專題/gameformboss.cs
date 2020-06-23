7d61db3db5fbc65a01ce0d49dad693116131fa34
Kayxue <s0990006@gmail.com>
2020-06-23 13:17:16 +0800

    2020-06-23

---
+++ 資訊專題/gameformboss.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 資訊專題
+{
+    public partial class gameformboss : Form
+    {
+        Point x;
+        public gameformboss()
+        {
+            InitializeComponent();
+        }
+
+        private void gameformboss_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Size getgameformsize
+        {
+            set
+            {
+                Size = value;
+            }
+        }
+
+        public Point getformposition
+        {
+            set
+            {
+                x = value;
+            }
+        }
+    }
+}
